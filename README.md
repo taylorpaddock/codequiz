207c8070cb6afec8cfb2cafb52d22d9ebf068268
Taylor Paddock <tayjeep@comcast.net>
2021-08-30 23:41:41 -0700

    updated files for css, html, js and ReadME

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # codequiz
+
+## Description
+
+Create a code quiz with a start screen and 4 questions. You have 75 seconds to finish the quiz. Each question you guess incorrectly takes off 10 seconds from the clock. Each question you answer correctly adds 10 points to your score. If the timer runs out before you answer all th questions then the quiz ends.
+
+At the end of the quiz you can save your initials and your score.
+
+## Installation
+
+Website can be accessed through my GitHub profile.
+
+## Usage
+
+Website application can be viewed on all screen sizes.
+link: https://taylorpaddock.github.io/codequiz/
+
+```md
+![website screenshot](/assets/images/code-quiz.jpg)
+```
+
+## Credits
+
+Used help from:
+https://www.sitepoint.com/simple-javascript-quiz/
+https://www.codingnepalweb.com/quiz-app-with-timer-javascript/
+
+## How to Contribute
+
+Update code quiz to properly read correct answers and capture score. Print score and initals at the end of the game.
+
+
+
---
+++ script.js
@@ -0,0 +1,113 @@
+var buttonStart = document.getElementById("button-start"); 
+var start = document.getElementById("start"); 
+var quiz = document.getElementById("quiz");
+var quizQuestions = document.getElementById("paragraph");
+var timeCounter = document.getElementById("counter");
+var questionButtons = document.getElementById("question-buttons");
+var timer = document.getElementById("timer");
+var counter = 75;
+var userScore = document.getElementById("score");
+var buttonScore = document.getElementById("button-score");
+var score = 0; //questionNumber
+var nextQuestion;
+var allScores = [];
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+var addScore = document.getElementById("addscore");
+
+var currentIndex = 0;
+var questions = [
+  {
+    paragraph : "What are the data types supported by JavaScript?",
+    choices : ["Boolean", "Strings", "Numbers", "All the Above"],
+    correct : "All the Above",
+  },
+  {
+    paragraph : "Which built-in method reverses the order of the elements of an array?",
+    choices : ["changeOrder(order)", "reverse()", "sort(order)", "function()"],
+    correct : "reverse()",
+  },
+  {
+    paragraph : "Which of the following function of Array object extracts a section of an array and returns a new array?",
+    choices : ["slice()", "shift()", "some()", "filter()"],
+    correct : "slice()",
+  },
+  {
+    paragraph : "Which of the following are capabilities of functions in JavaScript?",
+    choices : ["Return a value", "Accept parameters and return a value", "Accept parameters", "None of the Above"],
+    correct : "Accept parameters",
+  },
+
+];
+
+function startQuiz() {
+  if (storedScores !== null) {
+    allScores = storedScores;
+  }
+  start.style.display = "none";
+  quiz.style.display = "flex";
+  var nextQuestion = questions[currentIndex];
+  console.log(nextQuestion.paragraph);
+    
+    displayQuestion(nextQuestion);
+  
+  gameTimer();
+}
+
+buttonStart.addEventListener("click", startQuiz);
+
+function gameTimer() {
+  setInterval(function(){
+    timer.innerHTML = counter;
+    counter--;
+  }, 1000);
+}
+
+function displayQuestion(questions){
+  quizQuestions.innerHTML = questions.paragraph;
+  questions.choices.forEach(element => {
+  var button = document.createElement("BUTTON");
+  button.className = "button";
+  button.innerHTML = element;
+  questionButtons.appendChild(button);
+  button.addEventListener("click", displaynextQuestion);
+  });
+}
+
+function displaynextQuestion(e){
+  currentIndex++;
+  if(currentIndex < questions.length){
+      correctResponse(e.target.textContent == questions[currentIndex].correct);
+      questionButtons.innerHTML="";
+      if(currentIndex < questions.length){    
+          nextQuestion = questions[currentIndex];
+          displayQuestion(nextQuestion);
+      }else {
+          currentIndex = 0;
+          displayQuestion(nextQuestion);
+      }
+  }else{
+      console.log("endgame");
+      endGame();
+  }
+}
+
+function correctResponse(response){
+  if(response){
+      userScore = score + 10;
+      console.log("Good");
+  }else {
+      counter = counter - 10;
+      timer.innerHTML = counter;
+      console.log("Wrong");
+  }
+  setTimeout(function(){
+      alert.innerText="";
+      }, 1000);
+}
+
+function endGame() {
+  userScore.innerText = score;
+  addScore.style.display = "flex";
+  timer.style.display = "none";
+  quiz.style.display = "none";
+}
